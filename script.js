6687873605fc95de6aa02b78369e50cf633deec1
꧁༒• PROTIK •༒꧂ <69142530+ProtikKumarDas@users.noreply.github.com>
2021-10-05 18:51:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//This part is for typing animation
+var typed = new Typed(".text-slider", {
+    strings: ["Programmer","Web Developer","Python Guy","Designer","Gamer"],
+    typeSpeed: 70,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 50,
+});
